a9975c2d438c50c43e18b1a6350be00b5974f68b
iaingblack <iaingblack@gmail.com>
2016-10-01 11:48:09 +0100

    Create Centos7-Docker-Install-Script.sh

---
+++ Centos7-Docker-Install-Script.sh
@@ -0,0 +1,23 @@
+yum update
+
+tee /etc/yum.repos.d/docker.repo <<-'EOF'
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/7/
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+
+yum install docker-engine
+systemctl enable docker
+systemctl start docker
+
+curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+curl -L https://github.com/docker/machine/releases/download/v0.7.0/docker-machine-`uname -s`-`uname -m` > /usr/local/bin/docker-machine && \
+chmod +x /usr/local/bin/docker-machine
+
+docker-compose --version
+docker-machine version
