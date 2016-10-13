bda05c516b517f79ecf044e8b803ab8d9acb734d
iaingblack <iaingblack@gmail.com>
2016-10-13 10:23:26 +0100

    Update Windows-Server-2016-Docker-Installer.ps1

--- Windows-Server-2016-Docker-Installer.ps1
+++ Windows-Server-2016-Docker-Installer.ps1
@@ -3,6 +3,8 @@
 Install-WindowsFeature containers
 Restart-Computer -Force
 
+#--------------------------------------------------------------------
+
 # Download, install and configure Docker Engine
 Invoke-WebRequest "https://download.docker.com/components/engine/windows-server/cs-1.12/docker.zip" -OutFile "$env:TEMP\docker.zip" -UseBasicParsing
 
@@ -14,6 +16,20 @@ $env:path += ";c:\program files\docker"
 # For persistent use, will apply even after a reboot. 
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\Program Files\Docker", [EnvironmentVariableTarget]::Machine)
 
+# Open firewall port 2375
+netsh advfirewall firewall add rule name="docker engine" dir=in action=allow protocol=TCP localport=2375
+
+#------------------------------------------------------------------
+#CHOOSE ONE
+
 # Start a new PowerShell prompt before proceeding
 dockerd --register-service
+
+# Configure Docker daemon to listen on both pipe and TCP (replaces docker --register-service invocation above)
+dockerd.exe -H npipe:////./pipe/docker_engine -H 0.0.0.0:2375 --register-service
+
+#Then
 Start-Service docker
+
+#If you have it registred already and can't re-register, exit powerhsell and type this
+sc delete docker
